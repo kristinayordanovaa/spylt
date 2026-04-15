187e5746cec0db1de230ce381a2d85e7b239c109
Kristina Yordanova <kristinayordanovaa@gmail.com>
2026-04-15 07:28:29 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar"
+import HeroSection from "./sections/HeroSection"
+
+function App() {
+  
+
+  return (
+    <>
+      <main>
+        <Navbar />
+        <HeroSection />
+      </main>
+    </>
+  )
+}
+
+export default App
